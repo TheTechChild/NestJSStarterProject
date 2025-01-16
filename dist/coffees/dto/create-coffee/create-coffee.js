e8039e5faaf032b60e29002ac9f493d12bf3357d
Clayton-Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2025-01-16 12:43:43 -0700

    ignore dist folder

--- dist/coffees/dto/create-coffee/create-coffee.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateCoffee = void 0;
-class CreateCoffee {
-}
-exports.CreateCoffee = CreateCoffee;
-//# sourceMappingURL=create-coffee.js.map
