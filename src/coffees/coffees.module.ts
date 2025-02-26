2d4c15a0712c9988fbba3e2d43457f8dcccee289
Clayton-Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2025-02-26 14:58:24 -0700

    rewrite the imports in the coffees module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Event } from 'src/events/entities/event.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Coffee]),
-    TypeOrmModule.forFeature([Flavor]),
-  ],
+  imports: [ TypeOrmModule.forFeature([ Coffee, Flavor, Event ]) ],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
