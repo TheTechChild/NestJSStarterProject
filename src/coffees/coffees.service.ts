0a1668f16151623eecbfb07b73b6c0044047d02a
Clayton-Noyes <20774670+Clayton-Noyes@users.noreply.github.com>
2025-02-26 14:59:13 -0700

    import Event entity and Datasource library from typeorm and initialize a private readonly dataSource variable in the CoffeesService to be used in a queryRunner

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,11 +1,12 @@
 import { HttpException, HttpStatus, Injectable, NotFoundException, Query } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { Event } from 'src/events/entities/event.entity';
 
 @Injectable()
 export class CoffeesService {
@@ -15,6 +16,8 @@ export class CoffeesService {
 
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
+
+    private readonly dataSource: DataSource,
   ) {}
 
   async findAll(@Query() paginationQuery: PaginationQueryDto) {
